1698ee0da0dd911fc2668d1e1a9b52b4dead6a95
m2man <nmduy881994@gmail.com>
2024-01-18 05:21:56 +0000

    done

--- eval_mae.sh
+++ eval_mae.sh
@@ -1,8 +1,8 @@
 export CKT_PATH='/mnt/data/mount_4TBSSD/nmduy/mae_ckts'
 
-export CKT_SIZE='large'
+export CKT_SIZE='base'
 export PATCH='16'
 export ALGO='pitome'
-export RATIO='0.925'
+export RATIO='0.95'
 
 python mae/main_finetune.py --eval --resume ${CKT_PATH}/mae_finetuned_vit_${CKT_SIZE}.pth --model vit_${CKT_SIZE}_patch${PATCH} --batch_size 100 --ratio ${RATIO} --compress_method ${ALGO} 
