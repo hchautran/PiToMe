527d5fb4321f804f5caedc2f4dcf70f390bb380e
chau <tranhoaichau.00@gmail.com>
2024-01-24 03:33:44 +0100

    add ic vis

--- ic/visualize.py
+++ ic/visualize.py
@@ -1,4 +1,36 @@
 from main_pitome import main as pitome_runner
 from main_tome import main as tome_runner
+from main_diff import main  as diffrate_runner
+from main_pitome import get_args_parser as pitome_args 
+from main_tome import get_args_parser as tome_args 
+from main_diff import get_args_parser  as diff_args
+import argparse
 
-
+if __name__ == '__main__':
+    for algo in [
+        'tome',
+        'pitome',
+        'diffrate',
+        'none',
+    ]:
+        r = 0.9385
+        if algo == 'tome':
+            parser = argparse.ArgumentParser('DeiT training and evaluation script', parents=[tome_args()])
+            args = parser.parse_args()
+            tome_runner()
+        elif algo == 'pitome':
+            parser = argparse.ArgumentParser('DeiT training and evaluation script', parents=[pitome_args()])
+            args = parser.parse_args()
+            pitome_runner()
+        elif algo == 'diffrate':
+            parser = argparse.ArgumentParser('DeiT training and evaluation script', parents=[diff_args()])
+            args = parser.parse_args()
+            diffrate_runner()
+        else:
+            parser = argparse.ArgumentParser('DeiT training and evaluation script', parents=[diff_args()])
+            args = parser.parse_args()
+            diffrate_runner()
+            
+            
+    
+            
