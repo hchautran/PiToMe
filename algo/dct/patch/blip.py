78b8acf06f035b432d76124feb37dc811143f51b
chau <tranhoaichau.00@gmail.com>
2024-03-12 03:14:29 +0100

    update

---
+++ algo/dct/patch/blip.py
@@ -0,0 +1,156 @@
+import torch
+from lavis.models.vit import VisionTransformer, Attention, Block
+from ..merge import dc_transform 
+
+class DCTBlock(Block):
+    """
+    Modifications:
+     - Apply DCT between the attention and mlp blocks
+     - Compute and propogate token size and potentially the token sources.
+    """
+    
+    def compress_x(self, metric, x):
+        ratio = self._dct_info["ratio"].pop()
+        if ratio < 1.0:
+            x = dc_transform(x, ratio=ratio)
+
+        return x
+
+    def forward(self, x, register_hook=False):
+        # attn_size = self._dct_info["size"] if self._dct_info["prop_attn"] else None
+        # x_attn, metric, attn = self.attn(self.norm1(x), register_hook=register_hook)
+        # x = x + self.drop_path(x_attn)
+        # x = self.compress_x(metric, x) 
+        # x = x + self.drop_path(self.mlp(self.norm2(x)))
+        # return x
+        x = x + self.drop_path(self.attn(self.norm1(x), register_hook=register_hook))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        x = self.compress_x(x, x) 
+        # print(x.shape)
+        return x
+
+
+
+
+class DCTAttention(Attention):
+    """
+    Modifications:
+     - Apply proportional attention
+     - Return the mean of k over heads from attention
+    """
+
+
+    def forward(self, x, register_hook=False):
+        B, N, C = x.shape
+        qkv = (
+            self.qkv(x)
+            .reshape(B, N, 3, self.num_heads, C // self.num_heads)
+            .permute(2, 0, 3, 1, 4)
+        )
+        q, k, v = (
+            qkv[0],
+            qkv[1],
+            qkv[2],
+        )  # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        if register_hook:
+            self.save_attention_map(attn)
+            # attn.register_hook(self.save_attn_gradients)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x, k.mean(1), attn
+
+def make_dct_class(transformer_class):
+    class DCTVisionTransformer(transformer_class):
+        """
+        Modifications:
+        - Initialize r, token size, and token sources.
+        """
+
+        def forward_features(self, x, register_blk=-1) -> torch.Tensor:
+      
+            self._dct_info["r"] = [self.r]* len(self.blocks) 
+            self._dct_info["ratio"] = [self.ratio] * len(self.blocks) 
+            self._dct_info["size"] = None
+            self._dct_info["source"] = None
+            self.total_flop = 0
+
+            B = x.shape[0]
+            x = self.patch_embed(x)
+
+            cls_tokens = self.cls_token.expand(
+                B, -1, -1
+            )  # stole cls_tokens impl from Phil Wang, thanks
+            x = torch.cat((cls_tokens, x), dim=1)
+
+            x = x + self.pos_embed[:, : x.size(1), :]
+            x = self.pos_drop(x)
+
+            for i, blk in enumerate(self.blocks):
+                x = blk(x, register_blk == i)
+                self.total_flop += self.calculate_block_flop(x.shape)
+            x = self.norm(x)
+            return x
+
+
+        def calculate_block_flop(self, shape):
+            flops = 0
+            _, N, C = shape
+            mhsa_flops = 4*N*C*C + 2*N*N*C
+            flops += mhsa_flops
+            ffn_flops = 8*N*C*C
+            flops += ffn_flops
+            return flops
+
+    return DCTVisionTransformer
+
+
+def apply_patch(
+   model: VisionTransformer, trace_source: bool = False, prop_attn: bool = True, margin=0.9, use_k=False):
+    """
+    Applies DCT to this transformer. Afterward, set r using model.r.
+
+    If you want to know the source of each token (e.g., for visualization), set trace_source = true.
+    The sources will be available at model._dct_info["source"] afterward.
+
+    For proportional attention, set prop_attn to True. This is only necessary when evaluating models off
+    the shelf. For trianing and for evaluating MAE models off the self set this to be False.
+    """
+    DCTVisionTransformer = make_dct_class(model.__class__)
+    print('using', 'dct')
+
+    model.__class__ = DCTVisionTransformer
+    model.ratio = 1.0 
+    model.r=0.0
+    
+    # model.compress_method = 'dct' 
+    model._dct_info = {
+        "ratio": model.ratio,
+        "margin":  [],
+        "size": None,
+        "source": None,
+        "trace_source": trace_source,
+        "prop_attn": prop_attn,
+        "class_token": model.cls_token is not None,
+        "distill_token": False,
+    }
+    current_layer = 0
+    margin = margin 
+    num_layers = len(model.blocks)
+    # margins = [margin - margin*(i/num_layers) for i in range(num_layers)]
+
+    if hasattr(model, "dist_token") and model.dist_token is not None:
+        model._dct_info["distill_token"] = True
+
+    for module in model.modules():
+        if isinstance(module, Block):
+            # module.__class__ = DCTBlock if compress_method == 'dct' else PiDCTBlock 
+            module.__class__ = DCTBlock
+            module._dct_info = model._dct_info
+            current_layer +=1
---
+++ algo/dct/patch/timm.py
@@ -0,0 +1,111 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# --------------------------------------------------------
+
+
+from typing import Tuple
+
+import torch
+from timm.models.vision_transformer import Attention, Block, VisionTransformer
+from copy import copy
+
+from ..merge import dc_transform 
+
+
+class ToMeBlock(Block):
+    """
+    Modifications:
+     - Apply ToMe between the attention and mlp blocks
+     - Compute and propogate token size and potentially the token sources.
+    """
+
+    def _drop_path1(self, x):
+        return self.drop_path1(x) if hasattr(self, "drop_path1") else self.drop_path(x)
+
+    def _drop_path2(self, x):
+        return self.drop_path2(x) if hasattr(self, "drop_path2") else self.drop_path(x)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Note: this is copied from timm.models.vision_transformer.Block with modifications.
+        attn_size = self._dct_info["size"] if self._dct_info["prop_attn"] else None
+        x_attn, metric = self.attn(self.norm1(x), attn_size)
+        x = x + self._drop_path1(x_attn)
+        x = x + self._drop_path2(self.mlp(self.norm2(x)))
+        r = self._dct_info["r"].pop(0)
+        if r > 0:
+            x = dc_transform(x, k=r)
+
+        return x
+
+
+class ToMeBlockUsingRatio(Block):
+    """
+    Modifications:
+     - Apply ToMe between the attention and mlp blocks
+     - Compute and propogate token size and potentially the token sources.
+    """
+
+    def _drop_path1(self, x):
+        return self.drop_path1(x) if hasattr(self, "drop_path1") else self.drop_path(x)
+
+    def _drop_path2(self, x):
+        return self.drop_path2(x) if hasattr(self, "drop_path2") else self.drop_path(x)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Note: this is copied from timm.models.vision_transformer.Block with modifications.
+        attn_size = self._dct_info["size"] if self._dct_info["prop_attn"] else None
+        x_attn, metric = self.attn(self.norm1(x), attn_size)
+        x = x + self._drop_path1(x_attn)
+        x = x + self._drop_path2(self.mlp(self.norm2(x)))
+        ratio = self._dct_info["ratio"].pop(0)
+        if ratio < 1.0:
+            x = dc_transform(x, ratio=ratio)
+
+        return x
+
+
+class ToMeAttention(Attention):
+    """
+    Modifications:
+     - Apply proportional attention
+     - Return the mean of k over heads from attention
+    """
+
+    def forward(
+        self, x: torch.Tensor, size: torch.Tensor = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        # Note: this is copied from timm.models.vision_transformer.Attention with modifications.
+        B, N, C = x.shape
+        qkv = (
+            self.qkv(x)
+            .reshape(B, N, 3, self.num_heads, C // self.num_heads)
+            .permute(2, 0, 3, 1, 4)
+        )
+        q, k, v = (
+            qkv[0],
+            qkv[1],
+            qkv[2],
+        )  # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+
+        # Apply proportional attention
+        if size is not None:
+            attn = attn + size.log()[:, None, None, :, 0]
+
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        # Return k as well here
+        return x, k.mean(1)
+
