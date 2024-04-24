7a7682d73bcad45ff8fb0c92795a79a4df2deb92
chau <tranhoaichau.00@gmail.com>
2024-04-24 09:55:07 +0200

    update

---
+++ train_scripts/train_itr_albef.sh
@@ -0,0 +1,4 @@
+# LIB_PATH='/media/caduser/MyBook/chau/miniconda3/envs/PiToMe/lib/python3.11/site-packages'
+# DATASET = 'coco'
+DATASET=$1 # coco or flickr
+python -m torch.distributed.run --nproc_per_node=5 main_vl.py --cfg-path train_scripts/albef_itr_${DATASET}.yml --algo $2 --use_k False --ratio $3 --model albef 
