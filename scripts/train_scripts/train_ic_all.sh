d6488fc844894fbe15fc628f4a9015163bd2bc47
chau <tranhoaichau.00@gmail.com>
2024-10-31 08:53:18 +0100

    update

--- scripts/train_scripts/train_ic_all.sh
+++ scripts/train_scripts/train_ic_all.sh
@@ -1,7 +1,7 @@
 SIZE=S
 
-sh train_scripts/train_ic.sh DEIT $SIZE 224 1.0 none 5 &  
-sh train_scripts/train_ic.sh DEIT $SIZE  224 0.925 dct 2 &  
-sh train_scripts/train_ic.sh DEIT $SIZE  224 0.925 tofu 3 &  
-sh train_scripts/train_ic.sh DEIT $SIZE  224 0.925 tome 4 &  
+# sh scripts/train_scripts/train_ic.sh DEIT $SIZE 224 1.0 none 5 &  
+# sh scripts/train_scripts/train_ic.sh DEIT $SIZE  224 0.925 dct 2 &  
+# sh scripts/train_scripts/train_ic.sh DEIT $SIZE  224 0.925 tofu 3 &  
+sh scripts/train_scripts/train_ic.sh DEIT $SIZE  224 0.925 tome 4 &  
 sh scripts/train_scripts/train_ic.sh DEIT $SIZE 224 0.925 pitome 1 
