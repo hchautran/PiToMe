d7485fd037ab6417715715b9a3ecd76076050747
chau <tranhoaichau.00@gmail.com>
2024-11-25 10:04:41 +0100

    add vqa

--- scripts/eval_scripts/eval_vqa.sh
+++ scripts/eval_scripts/eval_vqa.sh
@@ -1,5 +1,5 @@
-export DATASET_PATH='/media/caduser/MyBook/chau/.cache'
-export HF_DATASETS_CACHE='/media/caduser/MyBook/chau/.cache'
+# export DATASET_PATH='/media/caduser/MyBook/chau/.cache'
+# export HF_DATASETS_CACHE='/media/caduser/MyBook/chau/.cache'
 # for tasks in 'coco_cap' 'scienceqa_full' 'vqav2' 'flickr30k'
 export model='llava-v1.5-7b'
 # for task in  'vqav2' 'vizwiz_vqa_val' 'mmbench' 'mme'
@@ -22,7 +22,7 @@ do
         do 
             for ratio in  '0.85' '0.875' '0.9' '0.925' '0.95' '0.975'  
             do 
-                python -m torch.distributed.run --nproc_per_node=5 lmms-eval/lmms_eval/__main__.py \
+                python -m torch.distributed.run --nproc_per_node=1 main_vqa.py \
                     --model llava   \
                     --model_args pretrained="liuhaotian/$model" \
                     --tasks $task --log_samples \
--- tasks/ vqa/lmms-eval/.github/pull_request_template.md
+++
@@ -1,8 +0,0 @@
-Before you open a pull-request, please check if a similar issue already exists or has been closed before.
-
-### When you open a pull-request, please be sure to include the following
-
-- [ ] A descriptive title: [xxx] XXXX
-- [ ] A detailed description
-
-Thank you for your contributions!
